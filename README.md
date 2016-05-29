bcf245912a160c6ee084a1d0f12edee9bfd30da0
james.moriarty <jamespaulmoriarty@gmail.com>
2016-05-29 19:25:40 +1000

    bugfix: null stdin.

--- README.md
+++ README.md
@@ -20,4 +20,16 @@ Run
 
 ```
 kotlin -cp robot.jar MainKt
+> PLACE WEST,1,1
+> REPORT
+WEST 1 1
+>
+```
+
+```
+kotlin -cp robot.jar MainKt < test.txt
+> > WEST 1 1
+> > WEST 0 1
+> > SOUTH 0 1
+> %                     
 ```
--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,7 @@
 import robot.Robot
 import robot.commands.CommandFactory
 import robot.commands.ICommand
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     var robot: Robot? = null
@@ -8,7 +9,8 @@ fun main(args: Array<String>) {
     while(true) {
         print("> ")
 
-        var tokens             = readLine()!!.toLowerCase().split("(,| )+".toRegex()).toTypedArray()
+        var input              = readLine() ?: exitProcess(1)
+        var tokens             = input.toLowerCase().split("(,| )+".toRegex()).toTypedArray()
         var command : ICommand = CommandFactory(tokens)
 
         robot = command.exec(robot)
