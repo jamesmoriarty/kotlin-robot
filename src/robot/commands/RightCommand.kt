6a74a8d0532f225aa3f246050403cfc08df28b51
James Moriarty <jamespaulmoriarty@gmail.com>
2016-06-04 14:07:26 +1000

    refinement: convert body.

--- src/robot/commands/RightCommand.kt
+++ src/robot/commands/RightCommand.kt
@@ -3,9 +3,7 @@ package robot.commands
 import robot.Robot
 
 class RightCommand(vararg args: String) : Command {
-    override fun exec(robot: Robot?): Robot? {
-        return robot?.let({
-            return it.copy(direction = it.direction.rotate(-1))
-        })
-    }
+    override fun exec(robot: Robot?): Robot? = robot?.let({
+        return it.copy(direction = it.direction.rotate(-1))
+    })
 }
