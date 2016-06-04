0132b13692c6be54b603dceadee9e9ee4a61488c
James Moriarty <jamespaulmoriarty@gmail.com>
2016-06-04 14:11:47 +1000

    refinement: more imute.

--- src/robot/commands/PlaceCommand.kt
+++ src/robot/commands/PlaceCommand.kt
@@ -4,12 +4,12 @@ import robot.Direction
 import robot.Robot
 
 class PlaceCommand(vararg args: String) : Command {
-    var direction: Direction = Direction.valueOf(args[1].toUpperCase())
-    var x: Int               = args[2].toInt()
-    var y: Int               = args[3].toInt()
+    val direction: Direction = Direction.valueOf(args[1].toUpperCase())
+    val x: Int               = args[2].toInt()
+    val y: Int               = args[3].toInt()
 
     override fun exec(robot: Robot?): Robot? {
-        var newRobot = Robot(direction, x, y)
+        val newRobot = Robot(direction, x, y)
 
         if (newRobot.isOnBoard()) {
             return newRobot
